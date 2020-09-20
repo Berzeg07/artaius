2411d79c3022604216b9603c31f0bd6364e44f7e
Aslan <berezg86@gmail.com>
2020-09-20 18:55:48 +0300

    Меню лаптоп, шапка пиксель перфект

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,4 +1,26 @@
 $(document).ready(function () {
     // alert(1);
     $('.select select').selectric();
+
+    $('.catalog-btn').click(function () {
+        $('.menu-laptop').fadeIn();
+    });
+
+    $('.dropdownmenu__close').click(function () {
+        $('.menu-laptop').fadeOut();
+    });
+
+    $('.menu-laptop__list li').click(function () {
+        $('.menu-laptop__list li').removeClass('is-active');
+        $(this).addClass('is-active');
+        $('.dropdownmenu_laptop').css('display', 'none');
+        $(this).find('.dropdownmenu_laptop').fadeIn();
+    });
+
+    $(window).resize(function () {
+        if (window.matchMedia('(min-width: 1280px)').matches) {
+            $('.menu-laptop').removeAttr('style');
+        }
+    });
+
 });
