f4a22bcb6a989c127b307d8db58e97e6ee7e7213
Aslan <berezg86@gmail.com>
2020-09-22 22:01:35 +0300

    Баннер на главной

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -49,4 +49,52 @@ $(document).ready(function () {
         $(this).next().slideToggle();
     });
 
+    $('.brends-list__modal').mCustomScrollbar({
+        axis: 'y',              // вертикальный скролл 
+        theme: 'dark',  // тема 
+        // scrollInertia: '330',   // продолжительность прокрутки, значение в миллисекундах 
+        setHeight: 264,         // высота блока (переписывает CSS) 
+        // mouseWheel: {
+        //     deltaFactor: 300    // кол-во пикселей на одну прокрутку колёсика мыши 
+        // }
+    });
+
+    var bannerSlider = undefined;
+
+    function initBannerSlider() {
+        var screenWidth = $(window).width();
+        if (screenWidth < 1280 && bannerSlider == undefined) {
+
+            var bannerSlider = new Swiper('.banner-block', {
+                slidesPerView: 1,
+                spaceBetween: 30,
+                loop: true,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+                breakpoints: {
+                    991: {
+                        slidesPerView: 1.1,
+                        spaceBetween: 10,
+                    },
+                    1079: {
+                        slidesPerView: 1.3,
+                        spaceBetween: 10,
+                    },
+                    1279: {
+                        slidesPerView: 1.6,
+                        spaceBetween: 10,
+                    }
+                },
+            });
+
+        } else if (screenWidth > 1279 && bannerSlider != undefined) {
+            bannerSlider.destroy();
+            bannerSlider = undefined;
+        }
+    }
+
+    initBannerSlider();
+
 });
